e7156b7aeccb73dc09bcc6af7926a2482630e6a9
Rayan Alsharaf <rayan.alsharaf@student.onslow.school.nz>
2026-04-21 21:34:00 +0000

    My initials

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,5 +5,17 @@
 struct SwiftPlayground {
     static func main() {
         print("Hello, world!")
+
+        // Prints Onslow College
+        print("Onslow College")
+
+        /* Task 1
+        My initials printed on the screen
+        */
+        print("RRR      A  ")
+        print("R  R    A A ")
+        print("RRR    AAAAA")
+        print("R  R   A   A")
+        print("R   R  A   A")
     }
 }
