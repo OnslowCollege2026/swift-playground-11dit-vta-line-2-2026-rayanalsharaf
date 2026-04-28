f5aea68498c93abdc1a0821bcf68f6403317950c
Rayan Alsharaf <rayan.alsharaf@student.onslow.school.nz>
2026-04-28 21:54:58 +0000

    task 1 maths task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,22 +4,42 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let x = 5
+        let y = 10
+    
+        let added = x + y
+        let subtracted = x - y
+        let multipled = x * y
+        let divided = x / y
+        let remander = x % y
 
-        // Prints Onslow College
-        print("Onslow College")
 
-        /* Task 1
-        My initials printed on the screen
-        */
-        print("RRR      A  ")
-        print("R  R    A A ")
-        print("RRR    AAAAA")
-        print("R  R   A   A")
-        print("R   R  A   A")
+        let added2 = y + x
+        let subtracted2 = y - x
+        let multipled2 = y * x
+        let divided2 = y / x
+        let remander2 = y % x
 
-        // First constant firstname
-        let firstName = "Rayan"
-        print(firstName)
+
+        print("\(x) + \(y) = \(added)")
+        print("\(x) - \(y) = \(subtracted)")
+        print("\(x) * \(y) = \(multipled)")
+        print("\(x) / \(y) = \(divided)")
+        print("\(x) % \(y) = \(remander)")
+
+
+        print("\(y) + \(x) = \(added2)")
+        print("\(y) - \(x) = \(subtracted2)")
+        print("\(y) * \(x) = \(multipled2)")
+        print("\(y) / \(x) = \(divided2)")
+        print("\(y) % \(x) = \(remander2)")
+
+     // Task 1 - define two constants.
+        let amount = 18.5
+        let tip = amount / 5
+
+     // Printing the tip
+        print(tip)
+
     }
 }
